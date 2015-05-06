a2808b3f5cf9abce4c675f903911b7552470d9e3
DylanMog <dyluu.m@gmail.com>
2015-05-06 18:51:46 -0300

    cambios
    cambios

--- js/script.js
+++ js/script.js
@@ -5,8 +5,16 @@ window.addEventListener("batterystatus", onBatteryStatus, false);
 
 function onBatteryStatus(info) {
     // Handle the online event
+	// Este evento se dispara cuando un porcentaje de la bateria cambia en al menos 1% o si uno desconecta o conecta el celular a cargar
+	
 	var element = document.getElementById('estadobateria');
-	element.innerHTML = 'Nivel: '+ info.level + " esta contectado: " + info.isPlugged + '<br />';
+	element.innerHTML = 'Porcentaje de bateria: '+ info.level + "%" + "se esta cargando: " + info.isPlugged + '<br />';
+
+	if (info.isPlugged == false){
+		document.body.style.background = red;
+	}else{
+		document.body.style.background = green;	
+	}
 
     //console.log("Level: " + info.level + " isPlugged: " + info.isPlugged);
 }
@@ -16,6 +24,8 @@ window.addEventListener("batterycritical", onBatteryCritical, false);
 
 function onBatteryCritical(info) {
     // Handle the battery critical event
+	// Este evento se dispara cuando el porcentaje de la bateria alcanzo el nivel critico (el valor es especifico en cada dispositivo)
+	
 	
 	var element = document.getElementById('bateriacritica');
 	element.innerHTML = 'Nivel critico de bateria: '+ info.level + "%\n recarge pronto!" + '<br />';
@@ -28,9 +38,10 @@ window.addEventListener("batterylow", onBatteryLow, false);
 
 function onBatteryLow(info) {
     // Handle the battery low event
+	// Este evento se dispara cuando el porcentaje de la bateria alcanzo el nivel de bateria baja (el valor es especifico en cada dispositivo)
 	
 	var element = document.getElementById('bateriabaja');
-	element.innerHTML = 'Nivel bajo de bateria: '+ info.level + "%" + '<br />';
+	element.innerHTML = 'Porcentaje de bateria: '+ info.level + "%" + "ponga a carga su telefono!" + '<br />';
 	
 	
     //alert("Battery Level Low " + info.level + "%");
